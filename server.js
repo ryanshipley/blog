fad668d8f1464201e127f180720daae0af4854d2
Ryan Shipley <ryanshipley23@gmail.com>
2022-09-09 08:42:28 -0400

    adds author controller

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
 
+const authorsController = require('./controllers/authors.js');
+app.use('/authors', authorsController);
+
 
 app.get('/', (req, res) => {
     res.render('index.ejs');
